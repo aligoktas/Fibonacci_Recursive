8e492b10634bf8a6845c299cceef2be1d404b41d
Ali Göktaş <aligoktass16@gmail.com>
2015-02-21 23:34:09 +0200

    Fibonacci Sayıları
    Fibonacci sayılarını recursive bulur

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Recursive
+{
+    class Program
+    {
+        public static Stopwatch watch = new Stopwatch();
+        static void Main(string[] args)
+        {
+            long deger = 0;
+            Console.WriteLine("Kaçıncı Fibonacci : ");
+            deger = Convert.ToInt32(Console.ReadLine());
+
+            watch.Start();
+                Console.WriteLine("{0} . Fibonacci: {1}", deger, fibonacciRecursive(deger));
+            watch.Stop();
+             
+            Console.WriteLine("Gecen sure: {0} ", watch.Elapsed.ToString());
+            Console.ReadLine();
+
+        }
+        public static long fibonacciRecursive(long girdi)
+        {
+            if (girdi == 1 || girdi == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return (fibonacciRecursive(girdi - 1) + fibonacciRecursive(girdi - 2));
+            }
+
+        }
+    }
+}
